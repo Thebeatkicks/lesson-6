abf678da15fcbc994bc6c812be15b46f868d7adf
Thebeatkicks <000henrik@gmail.com>
2025-04-08 11:00:30 +0200

    added listBook() and started with markAsRead.

--- main.js
+++ main.js
@@ -1,36 +1,6 @@
 const library = []; 
-
-const book = {
-    title: "Lord of the Ring", //addBook();
-    author: "Tolkien", //addAuthor();
-    isRead: true,
-}
-
-function addBook() {
-    
-    const title = prompt("Title of the book"),
-    const author = prompt("Author of the book"),
-    const isRead = prompt("is Read? (y/n)"),
-    
-    const newBook = {
-        title: title,
-        author: author,
-        isRead: isRead,
-    };
-
-
-    library.push(newBook);
-    alert("Book Added!");
-}
-
-function markAsRead(title) {
-    book.isRead(true); 
-}
-   
-
 let running = true;
-
-1
+   
 function menu() {
     while (running) {
         const message = parseFloat(
@@ -55,14 +25,51 @@ function menu() {
                 alert("See you later!")
                 break
         default:
-                alert("Invaliud choice.")
+                alert("Invalid option.")
         }
     }
 }
 
-menu();
-/* create an array that has objects in it? 
+function addBook() {
+    
+    const title = prompt("Title of the book");
+    const author = prompt("Author of the book");
+    const isReadInput = prompt("is Read? (y/n)").toLowerCase();
+    const isRead = isReadInput === "y";
+    
+    const newBook = {
+        title: title,
+        author: author,
+        isRead: isRead,
+    };
 
+
+    library.push(newBook);
+    alert("Book Added!");
+}
+
+function markAsRead(title) {
+    const book = library.find(book => book.title.toLowerCase() === title.toLowerCase());
+
+}
+
+function listBook() {
+    if (library.lenght === 0) {
+        alert("No books added yet");
+    } else {
+        console.log("Your books:")
+        for (let book of library) {
+            console.log(
+                `Title: ${book.title}, Author: ${book.author}, Read: ${book.isRead ? "Yes" : "No"}`
+            );
+        }
+    }
+       
+}
+
+menu();
+
+/*
 Instructions: 
 
 Create a small app to track books you’ve read or want to read (If you are not in to books, feel free to change it to movies). Use prompt and alert to simulate the menu. 
